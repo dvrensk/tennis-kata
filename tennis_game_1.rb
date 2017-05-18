a1fb4b1851cf025b991f496f0cf5a38e9943f345
David Vrensk <david@vrensk.com>
2017-05-18 21:38:50 +0200

    Extract won!

--- tennis_game_1.rb
+++ tennis_game_1.rb
@@ -20,11 +20,13 @@ class TennisGame1
   def score
     if deuce?
       deuce_as_text
-    elsif tie_break?
+    elsif won?
+      "Win for #{player_in_the_lead.name}"
+    elsif tie_break_or_won?
       if points_delta.abs == 1
-        "Advantage #{winner_name}"
+        "Advantage #{player_in_the_lead.name}"
       else
-        "Win for #{winner_name}"
+        "Win for #{player_in_the_lead.name}"
       end
     else
       [
@@ -42,17 +44,17 @@ class TennisGame1
     player1.points - player2.points
   end
 
-  def winner_name
+  def player_in_the_lead
     if player1.points > player2.points
-      player1.name
+      player1
     elsif player1.points < player2.points
-      player2.name
+      player2
     else
       raise "hell"
     end
   end
 
-  def tie_break?
+  def tie_break_or_won?
     player1.points >= 4 || player2.points >= 4
   end
 
@@ -62,6 +64,10 @@ class TennisGame1
     else
       "Deuce"
     end
+  end
+
+  def won?
+    points_delta >= 2 && player_in_the_lead.points >= 4
   end
 
   POINTS_AS_TEXT = %w[Love Fifteen Thirty Forty]
