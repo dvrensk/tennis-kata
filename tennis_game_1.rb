1e69c6684f50737b89788e78247db0957a2b553e
David Vrensk <david@vrensk.com>
2017-05-18 20:43:26 +0200

    Consistent naming

--- tennis_game_1.rb
+++ tennis_game_1.rb
@@ -3,29 +3,29 @@ class TennisGame1
   def initialize(player1_name, player2_name)
     @player1_name = player1_name
     @player2_name = player2_name
-    @p1points = 0
-    @p2points = 0
+    @player1_points = 0
+    @player2_points = 0
   end
 
   def won_point(player_name)
     if player_name == @player1_name
-      @p1points += 1
+      @player1_points += 1
     else
-      @p2points += 1
+      @player2_points += 1
     end
   end
 
   def score
     result = ""
     tempScore=0
-    if (@p1points==@p2points)
+    if (@player1_points==@player2_points)
       result = {
           0 => "Love-All",
           1 => "Fifteen-All",
           2 => "Thirty-All",
-      }.fetch(@p1points, "Deuce")
-    elsif (@p1points>=4 or @p2points>=4)
-      minusResult = @p1points-@p2points
+      }.fetch(@player1_points, "Deuce")
+    elsif (@player1_points>=4 or @player2_points>=4)
+      minusResult = @player1_points-@player2_points
       if (minusResult==1)
         result ="Advantage " + @player1_name
       elsif (minusResult ==-1)
@@ -38,10 +38,10 @@ class TennisGame1
     else
       (1...3).each do |i|
         if (i==1)
-          tempScore = @p1points
+          tempScore = @player1_points
         else
           result+="-"
-          tempScore = @p2points
+          tempScore = @player2_points
         end
         result += {
             0 => "Love",
