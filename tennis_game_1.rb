4692fc69d9f3a2ed96dab201681bdb348ce50d00
David Vrensk <david@vrensk.com>
2017-05-18 21:42:09 +0200

    real tie break!

--- tennis_game_1.rb
+++ tennis_game_1.rb
@@ -22,12 +22,8 @@ class TennisGame1
       deuce_as_text
     elsif won?
       "Win for #{player_in_the_lead.name}"
-    elsif tie_break_or_won?
-      if points_delta.abs == 1
-        "Advantage #{player_in_the_lead.name}"
-      else
-        "Win for #{player_in_the_lead.name}"
-      end
+    elsif tie_break?
+      "Advantage #{player_in_the_lead.name}"
     else
       [
         points_to_text(player1.points),
@@ -54,8 +50,9 @@ class TennisGame1
     end
   end
 
-  def tie_break_or_won?
-    player1.points >= 4 || player2.points >= 4
+  def tie_break?
+    points_delta.abs == 1 &&
+    (player1.points >= 4 || player2.points >= 4)
   end
 
   def deuce_as_text
@@ -67,7 +64,7 @@ class TennisGame1
   end
 
   def won?
-    points_delta >= 2 && player_in_the_lead.points >= 4
+    points_delta.abs >= 2 && player_in_the_lead.points >= 4
   end
 
   POINTS_AS_TEXT = %w[Love Fifteen Thirty Forty]
